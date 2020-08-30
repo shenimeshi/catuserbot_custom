0609cc477809ad4c36bb1914ac397dc1305b39c9
Chanaka Chathuranga <chanakac.91@gmail.com>
2020-08-30 13:37:24 +0530

    Customized gm and gn arts

--- userbot/plugins/greetings.py
+++ userbot/plugins/greetings.py
@@ -1,10 +1,10 @@
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="gnt$"))
+@borg.on(admin_cmd(pattern="gn$"))
 async def gn(event):
-    await event.edit("｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･\n╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╭╮╭╮\n╭━┳━┳━┳╯┃╭━┳╋╋━┫╰┫╰╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃┃╋┃┃┃╭┫\n┣╮┣━┻━┻━╯╰┻━┻╋╮┣┻┻━╯\n╰━╯╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥° ♥｡･ﾟ♡ﾟ･")
+    await event.edit(".,-,*´'*°☆\n/.(\n\ { ۰۪۫G۪۫۰۰۪۫O۪۫۰۰۪۫O۪۫۰۰۪۫D۪۫۰ ۰۪۫N۪۫۰۰۪۫I۪۫۰۰۪۫G۪۫۰۰۪۫H۪۫۰۰۪۫T۪۫۰\n-`☆.¸¸.❤️")
     
     
-@borg.on(admin_cmd(pattern="gmg$"))
+@borg.on(admin_cmd(pattern="gm$"))
 async def gm(event):
-    await event.edit("｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･\n╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮\n╭━┳━┳━┳╯┃╭━━┳━┳┳┳━┳╋╋━┳┳━╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃╋┃╭┫┃┃┃┃┃┃┃╋┃\n┣╮┣━┻━┻━╯╰┻┻┻━┻╯╰┻━┻┻┻━╋╮┃\n╰━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･")
+    await event.edit("**☆❋──❃ ɢ∞פ  ʍօ®ɴɪɴɢ ❃──❋☆**")
